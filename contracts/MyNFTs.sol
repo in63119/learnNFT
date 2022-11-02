38b61927f725142510b6bbd941e694aa08457a29
이인수 <in63119@gmail.com>
2022-11-02 14:57:31 +0900

    first setting

---
+++ contracts/MyNFTs.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract MyNFTs is ERC721URIStorage, Ownable {
+    using Counters for Couters.Counter;
+    Couters.Counter private _tokenIds;
+
+    constructor() public ERC721("MyNFTs", "MNFT") {}
+
+    function mintNFT(string memory tokenURI)
+        public onlyOwner
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+}
